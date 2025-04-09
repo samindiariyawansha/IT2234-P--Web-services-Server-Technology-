d9754bba2e4c2007e12db00a6296c588dadbb477
samindiariyawansha <sawiariyawansha101@gmail.com>
2025-04-09 22:39:39 +0530

    Add files via upload

---
+++ studentdb.js
@@ -0,0 +1,8 @@
+let  students = [
+    {regno:"2021ICT01",Name:"Sawi",age:25,gender:"female"},
+    {regno:"2021ICT02",Name:"Nuwani",age:24,gender:"female"},
+    {regno:"2021ICT03",Name:"Abi",age:26,gender:"female"},
+    {regno:"2021ICT04",Name:"Wijeykoon",age:25,gender:"male"},
+    {regno:"2021ICT05",Name:"sithu",age:23,gender:"female"}
+];
+module.exports = students;
